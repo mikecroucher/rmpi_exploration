878924af61b82c4cd79f88e15e422824305e61cd
Mike Croucher <michael.p.croucher@googlemail.com>
2017-06-09 09:16:15 +0100

    Changed to R CMD so that its identical to alces

--- sharc/gcc/run_on_sharc_gcc.sh
+++ sharc/gcc/run_on_sharc_gcc.sh
@@ -6,6 +6,4 @@ module load apps/R/3.4.0/gcc-4.8.5
 module load dev/gcc/4.9.4                 
 module load mpi/openmpi/2.0.1/gcc-4.9.4
 
-mpirun -np 1 Rscript example1.r
-#Rscript example1.r
-
+mpirun -np 1 R CMD BATCH ./example1.r ./out.txt
